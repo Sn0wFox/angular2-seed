994d3ee2b8d1b7f82a8ab4d4d7bf5d14c3a6a518
Sn0wFox <ruben.pericas@laposte.net>
2017-01-15 21:18:22 +0100

    Added support for import from custom lib client side

--- src/client/app/about/about.component.ts
+++ src/client/app/about/about.component.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
+import {OnInit} from '@angular/core';
+
+import * as Lib from '../../../lib/lib-test';
 
 @Component({
   selector: 'about',
   styleUrls: ['./about.component.css'],
   templateUrl: './about.component.html'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  protected info: string;
+  
+  ngOnInit(): void {
+    Lib
+      .myFunction()
+      .then((info: string) => {
+        this.info = info;
+      });
+  }
 }
