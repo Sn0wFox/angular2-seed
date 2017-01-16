4774ea016d4d4906df53f120d28b9d209c84196e
Sn0wFox <ruben.pericas@laposte.net>
2017-01-16 18:57:55 +0100

    Added server side for mmg

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,18 +1,24 @@
 import * as Bluebird from "bluebird";
 import * as express from "express";
 import * as path from "path";
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
 
 import * as Lib from "../lib/lib-test";
+
+import apiRouter from "./routes/server.api-router";
+import mainRouter from "./routes/server.main-router";
+import {serverPort, staticRoot} from "./server.config";
 
 const app: any = express();
 
 console.log(path.resolve(__dirname + "/.."));
 
-app.use(express.static(path.resolve(__dirname + "/../client")));
+//app.use(express.static(path.resolve(__dirname + "/../client")));
 
 app.set('port', process.env.PORT || 3000);
 
-app.get("/api", (req: any, res: any, next: any) => {
+app.get("/api/tralala", (req: any, res: any, next: any) => {
   Lib
     .myFunction()
     .then((hello: string) => {
@@ -24,6 +30,17 @@ app.get("/api", (req: any, res: any, next: any) => {
     });
 });
 
+// Configure the server to parse body
+app.use(cookieParser("ManManGa awesome app!"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Configure routes
+// NOTE: globalRouter must be the last used, since it defines defaults routes
+app.use(apiRouter);
+app.use(mainRouter);
+
+// Start the server
 app.listen(app.get('port'), function() {
   console.log('Express server started at http://localhost:' + app.get('port'));
 });
